ea2e39b0ec379c20f435515e10dc04e642322ea8
MSME Dost <msmedost.dev@gmail.com>
2024-07-02 11:16:04 +0530

    first commit

---
+++ src/component/CardItem/CardItem.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import './CardItem.css'
+
+import { motion } from 'framer-motion'
+import { fadeIn } from '../../variants';
+import { Link } from 'react-router-dom';
+
+
+const CardItem = (props) => {
+    const { cardDetails } = props
+    const { id, name, businesscategory, yourphoto } = cardDetails
+
+    return (
+        <motion.li
+
+            variants={fadeIn("up", 0.2)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="cus-profile-container">
+                
+            <img src='https://img.freepik.com/premium-photo/user-icon-glossy-blue_335552-1305.jpg?w=740'
+                alt="profile"
+                className="card-profile"
+            />
+            <div className='name-positon-container'>
+                <p className="card-name">{name}</p>
+                <p className="card-possition">{businesscategory}</p>
+                <p className="card-possition">{id}</p>
+            </div>
+{/*             <Link to={`/forms/${id}`}><button className='view-more-btn'>View More</button></Link> */}
+
+        </motion.li>
+    )
+
+}
+
+export default CardItem
---
+++ src/component/Listedmember/Listedmember.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
+import CardItem from '../CardItem/CardItem';
+import './listedmember.css'
+
+
+
+function Listedmember() {
+    const [users, setUsers] = useState([]);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/api/forms')
+            .then(response => {
+                setUsers(response.data);
+            })
+            .catch(error => {
+                console.error('Error fetching the users:', error);
+                setError('Error fetching the users');
+            });
+    }, []);
+
+    return (
+        <ul className='listed-member-ul-list-container'>
+            {users.map(eachCard => (
+                <CardItem cardDetails={eachCard} key={eachCard.id} />
+            ))}
+        </ul>
+    );
+}
+
+export default Listedmember
